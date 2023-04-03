77c0be7ade65676c52dc8b0f570209183d6d839b
Katherine928 <katherine71317@gmail.com>
2023-04-03 13:08:26 -0400

    init commit

---
+++ src/test/java/net/katherine/springbootkafkaproject/SpringbootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.katherine.springbootkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
