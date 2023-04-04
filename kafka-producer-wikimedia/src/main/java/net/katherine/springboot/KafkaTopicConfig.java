2db1eb8d19bf29297306e03df2ee2ff72d9b68da
Katherine928 <katherine71317@gmail.com>
2023-04-04 15:40:03 -0400

    Code Refactor

--- kafka-producer-wikimedia/src/main/java/net/katherine/springboot/KafkaTopicConfig.java
+++ kafka-producer-wikimedia/src/main/java/net/katherine/springboot/KafkaTopicConfig.java
@@ -1,12 +1,17 @@
 package net.katherine.springboot;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
     @Bean
     public NewTopic topic() {
 
